053798b4317a014e02dcad93042aed682cec79b5
aiqualitylab <sreekanth.harigovindan@outlook.com>
2026-05-03 12:52:38 +0300

    feat(refactor): add newly generated file and remove some old files

--- cypress/e2e/generated/01_test-login_20260503_124242.cy.js
+++ cypress/e2e/generated/01_test-login_20260503_124242.cy.js
@@ -3,16 +3,18 @@
 describe('Login Tests', function () {
 
     function getSelector(selectorEntry) {
-        if (!selectorEntry) {
-            return null;
-        }
-        if (typeof selectorEntry === 'string') {
-            return selectorEntry;
-        }
-        if (typeof selectorEntry === 'object') {
-            return selectorEntry.fallback_css || null;
-        }
+        if (!selectorEntry) { return null; }
+        if (typeof selectorEntry === 'string') { return selectorEntry; }
+        if (typeof selectorEntry === 'object') { return selectorEntry.fallback_css || null; }
         return null;
+    }
+
+    function buildUrl(baseUrl, pattern, params) {
+        let url = baseUrl + pattern;
+        Object.keys(params || {}).forEach(function (key) {
+            url = url.replace('{' + key + '}', encodeURIComponent(params[key]));
+        });
+        return url;
     }
 
     function fillFormFields(testCase, selectors) {
@@ -33,30 +35,30 @@ describe('Login Tests', function () {
     });
 
     it('should succeed with valid data', function () {
-        cy.visit(this.testData.url);
         const valid = this.testData.test_cases.find(tc => tc.name === 'valid_test');
         const selectors = this.testData.selectors;
+        cy.visit(this.testData.url);
 
         fillFormFields(valid, selectors);
 
         const submitSelector = getSelector(selectors.submit);
         cy.get(submitSelector).click();
 
-        const successSelector = getSelector(selectors.success_container) || getSelector(selectors.error_container) || '#flash';
+        const successSelector = getSelector(selectors.success_container) || '#flash';
         cy.get(successSelector).should('be.visible');
     });
 
     it('should fail with invalid data', function () {
-        cy.visit(this.testData.url);
         const invalid = this.testData.test_cases.find(tc => tc.name === 'invalid_test');
         const selectors = this.testData.selectors;
+        cy.visit(this.testData.url);
 
         fillFormFields(invalid, selectors);
 
         const submitSelector = getSelector(selectors.submit);
         cy.get(submitSelector).click();
 
-        const errorSelector = getSelector(selectors.error_container) || getSelector(selectors.success_container) || '#flash';
+        const errorSelector = getSelector(selectors.error_container) || '#flash';
         cy.get(errorSelector).should('be.visible');
         cy.get(errorSelector).invoke('text').then((text) => {
             expect(text.toLowerCase()).to.match(/invalid|error|incorrect|failed/);
